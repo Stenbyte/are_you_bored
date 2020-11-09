095fcb559f4a348493b40d3a2126825c3795864b
Staffan Strömsholm <staffanstromsholm@MacBook-Pro-som-tillhor-Staffan.local>
2020-11-09 23:23:30 +0200

    wrote basic fetch function for tmdb api

--- js/script.js
+++ js/script.js
@@ -66,8 +66,8 @@ const spotifySearch = async () => {
             return `<li><a href=${spotifyURIs[i]}>${item.track.name}</a></li>`
         })
         document.getElementById('category-list').innerHTML = html;
-        
-        return data.items;
+
+        // return data.items;
 
     }
     getPlaylistByGenre(token, genreId)
@@ -81,6 +81,57 @@ musicBoxes.forEach(box => box.addEventListener('click', (e) => {
     genreId = e.target.innerText.toLowerCase();
     spotifySearch();
 }))
+
+// =========== TMDB API ==============
+const movieGenres = {
+    drama: 18,
+    action: 28,
+    adventure: 12,
+    comedy: 35,
+    fantasy: 14,
+    horror: 27
+}
+
+//Genre ids in TMBD
+// 0: {id: 28, name: "Action"}
+// 1: {id: 12, name: "Adventure"}
+// 2: {id: 16, name: "Animation"}
+// 3: {id: 35, name: "Comedy"}
+// 4: {id: 80, name: "Crime"}
+// 5: {id: 99, name: "Documentary"}
+// 6: {id: 18, name: "Drama"}
+// 7: {id: 10751, name: "Family"}
+// 8: {id: 14, name: "Fantasy"}
+// 9: {id: 36, name: "History"}
+// 10: {id: 27, name: "Horror"}
+// 11: {id: 10402, name: "Music"}
+// 12: {id: 9648, name: "Mystery"}
+// 13: {id: 10749, name: "Romance"}
+// 14: {id: 878, name: "Science Fiction"}
+// 15: {id: 10770, name: "TV Movie"}
+// 16: {id: 53, name: "Thriller"}
+// 17: {id: 10752, name: "War"}
+// 18: {id: 37, name: "Western"}
+// `https://api.themoviedb.org/3/discover/movie?with_genres=${movieGenres[drama]}&sort_by=revenue.desc&api_key=0a77033036d0112b03d5fb8d85f886b1&query=furious`
+
+const url = `https://api.themoviedb.org/3/discover/movie?with_genres=${movieGenres['drama']}&sort_by=revenue.desc&api_key=0a77033036d0112b03d5fb8d85f886b1`;
+
+fetch(url)
+.then(res => res.json())
+.then((data)=>console.log(data))
+.catch(err=>console.log(err))
+
+
+
+// let input = 'star wars'
+
+// const omdbSearch = async () => {
+//     const result = await fetch(`http://www.omdbapi.com/?t=${input}&apikey=2fe1037f`)
+//     const data = await result.json();
+//     console.log(data);
+// }
+
+// omdbSearch();
 
 const closePopup = () => {
     popupModal.classList.add('hide');
